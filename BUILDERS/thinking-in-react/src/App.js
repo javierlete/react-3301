239b80fd515c4a3e63f146f504ccc5326c56cd1d
Javier Lete <javier@eskura21.com>
2025-12-03 19:08:25 +0100

    Step 2.1: Crear maqueta sin nada más que HTML separado por componentes

--- BUILDERS/thinking-in-react/src/App.js
+++ BUILDERS/thinking-in-react/src/App.js
@@ -2,7 +2,73 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Aplicación</h1>
+    <main>
+      <FilterableProductTable />
+    </main>
+  );
+}
+
+function FilterableProductTable() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable />
+    </>
+  );
+}
+
+function SearchBar() {
+  return (
+    <>
+      <div>
+        <input type="search" placeholder="Search..."></input>
+      </div>
+      <div>
+        <input type="checkbox"></input>
+        <label for="instock">Only products in stock</label>
+      </div>
+    </>
+  );
+}
+
+function ProductTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+      </tbody>
+    </table>
+  );
+}
+
+function ProductCategoryRow() {
+  return (
+    <tr class="categoria">
+      <th colspan="2">Frutos</th>
+    </tr>
+  );
+}
+
+function ProductRow() {
+  return (
+    <tr>
+      <td className="outofstock">Plátano</td>
+      <td>5</td>
+    </tr>
   );
 }
 
